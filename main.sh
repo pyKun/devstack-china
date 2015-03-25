37138a5939adae0968726e31726ae8a2add1d25a
Kun Huang <gareth@openstacker.org>
2015-03-25 14:57:20 +0800

    a little fix on main.sh

--- main.sh
+++ main.sh
@@ -13,6 +13,8 @@ sudo apt-get install vim git tmux python-pip ipython
 sudo pip install -U pip # pip 6.0.0+ version has many important features
 cp pip.conf ~/.pip/
 
+# TODO setup pip caches
+
 # set up devstack
 cd ~
 git clone --depth 1 https://github.com/openstack/devstack.git
